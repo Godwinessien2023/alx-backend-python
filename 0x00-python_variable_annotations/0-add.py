c4d10082a3ac704da552b3ce4ecde0fa820de942
Godwin Essien <kinggodwin4u@yahoo.com>
2024-10-10 19:38:31 +0100

    chmod u+x

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-"""
-A type-annotated function
-"""
-
+"""A type-annotated function"""
 
 def add(a: float, b: float) -> float:
-    """
-    Takes a float a and a float b as arguments and returns their sum as a float
-    """
+    """add 2 integers"""
     return a + b
